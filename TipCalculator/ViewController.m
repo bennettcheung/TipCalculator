229f32f0131ddbdc32ac9c0ef738c9567a8fc9a5
Bennett <bennettcheung@yahoo.com>
2018-08-10 11:35:33 -0700

    Created project and added most features including filtering for numbers only in textfields

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentageTextField;
+@property (weak, nonatomic) IBOutlet UIButton *calculateTipButton;
 
 @end
 
@@ -17,6 +21,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.tipAmountLabel.text = @"";
 }
 
 
@@ -25,5 +30,32 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)calculateTip:(UIButton *)sender {
+    
+    
+    float tip = [self.billAmountTextField.text floatValue] * [self.tipPercentageTextField.text floatValue]/100;
+    self.tipAmountLabel.text =  [NSString stringWithFormat:@"$ %.2f", tip];
+    
+}
 
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self calculateTip:self.calculateTipButton];
+    return YES; // want to hide keyboard
+
+}
+
+-(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    
+    NSCharacterSet *allowedCharacters = [NSCharacterSet characterSetWithCharactersInString:@".0123456789"];
+    
+    if([string rangeOfCharacterFromSet:allowedCharacters.invertedSet].location == NSNotFound){
+        
+        return YES;
+        
+    }
+    
+    return NO;
+    
+}
 @end
